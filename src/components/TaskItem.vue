eb16bd84853441f6ffa239a2cf2a117ca0e0ed37
Diana397248 <68861982+Diana397248@users.noreply.github.com>
2024-12-13 23:51:11 +0400

    createTodo

--- src/components/TaskItem.vue
+++ src/components/TaskItem.vue
@@ -7,9 +7,13 @@ const props = defineProps({
 </script>
 
 <template>
-  <h2> {{ props.todo.name }} </h2>
+  <div class="task">
+    <h2> {{ props.todo.name }} </h2>
+  </div>
 </template>
 
 <style scoped>
-
+.task {
+  background-color: lightgray;
+}
 </style>
