00677ee3def7107b9f9a9999e91112fe2295c561
Diana397248 <68861982+Diana397248@users.noreply.github.com>
2024-12-13 21:39:14 +0400

    Отображение дел

---
+++ src/components/TaskList.vue
@@ -0,0 +1,13 @@
+<script setup>
+import TaskItem from '../components/TaskItem.vue';
+
+let todos = [{name: "постирать"}, {name: "сходить в магазин"}];
+</script>
+
+<template>
+  <TaskItem v-for="todo in todos" :todo="todo"/>
+</template>
+
+<style scoped>
+
+</style>
